384ee153b4f1e9627761bc14f87f5ba0e1f5d848
welentsai <welentsai@gmail.com>
2025-03-02 22:37:08 +0800

    topic: Lists in React

--- my-road-to-react/src/App.tsx
+++ my-road-to-react/src/App.tsx
@@ -3,6 +3,25 @@
 // import viteLogo from '/vite.svg';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 const a = ['a', 'b', 'c', 'd', 'e'];
 
 const welcome = {
@@ -20,16 +39,27 @@ function App() {
   return (
     <>
       <div>
-        <h1>Hello {getTitle(welcome.title)}</h1>
-        <h1>
-          {welcome.greeting} {welcome.title}
-        </h1>
-        <label htmlFor="search">Search: </label>
-        <input id="search" type="text" />
+        <h1>My Hacker Stories</h1>
 
-        {a.map((item, index) => (
-          <div key={index}>{item}</div>
-        ))}
+        <label htmlFor="Search">Search: </label>
+        <input type="text" id="Search" />
+
+        <hr />
+
+        <ul>
+          {list.map(item => (
+            // JSX to return
+            <li key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+              {item.title}
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
