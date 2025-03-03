d33d543e8369822dcee6b19c01d9c96f5402efa8
welentsai <welentsai@gmail.com>
2025-03-03 14:27:39 +0800

    topic: add react props

--- my-road-to-react/src/App.tsx
+++ my-road-to-react/src/App.tsx
@@ -1,26 +1,35 @@
 import { ChangeEvent } from 'react';
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+type Story = {
+  title: string;
+  url: string;
+  author: string;
+  num_comments: number;
+  points: number;
+  objectID: number;
+};
 
 const App = () => {
+  const stories: Array<Story> = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
@@ -29,7 +38,7 @@ const App = () => {
 
       <hr />
 
-      <List />
+      <List list={stories} />
     </div>
   );
 };
@@ -49,21 +58,32 @@ const Search = () => {
   );
 };
 
-const List = () => (
+type ListProps = {
+  list: Story[];
+};
+
+const List = ({ list }: ListProps) => (
   <ul>
     {list.map(item => (
-      // JSX to return
-      <li key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-        {item.title}
-      </li>
+      <Item key={item.objectID} item={item} />
     ))}
   </ul>
+);
+
+type ItemProps = {
+  item: Story;
+};
+
+const Item = ({ item }: ItemProps) => (
+  <li>
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+    {item.title}
+  </li>
 );
 
 export default App;
