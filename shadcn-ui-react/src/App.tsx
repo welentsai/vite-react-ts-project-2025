131133afc2d5f8de112fec13f5093e343c9eb960
welentsai <welentsai@gmail.com>
2025-03-23 23:31:51 +0800

    feat: my first shadcn button

--- shadcn-ui-react/src/App.tsx
+++ shadcn-ui-react/src/App.tsx
@@ -2,29 +2,23 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import { Button } from '@/components/ui/button';
 
 function App() {
   const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    console.log('handle click !!');
+  };
 
   return (
     <>
       <div>
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div className="flex min-h-svh flex-col items-center justify-center">
+          <Button onClick={handleClick}>Click me</Button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
 }
