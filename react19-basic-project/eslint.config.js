ca8113d512790016d2746076f6eb3bd305a26e2a
welentsai <welentsai@gmail.com>
2025-03-24 23:26:14 +0800

    feat: add shadcn/ui button

--- react19-basic-project/eslint.config.js
+++ react19-basic-project/eslint.config.js
@@ -1,10 +1,10 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
-import prettierConfig from 'eslint-config-prettier';
-import prettierPlugin from 'eslint-plugin-prettier';
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import prettierConfig from 'eslint-config-prettier'
+import prettierPlugin from 'eslint-plugin-prettier'
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -34,4 +34,4 @@ export default tseslint.config(
   {
     ignores: ['dist/', 'node_modules/'],
   }
-);
+)
